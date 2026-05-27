19c77123dceb283d8960c0e08fe0e22663733d17
Aruna-08/python <arunasri.g@srsconsultinginc.com>
2026-05-27 17:03:34 +0530

    second commit

---
+++ fizbuzz.py
@@ -0,0 +1,17 @@
+for num in range (1,20):
+  
+     if num % 2 == 0 and num % 4 == 0:
+  
+         print("Fizzbuzz")
+
+     elif num % 2 == 0:
+
+           print("Fizz")
+
+     elif num % 4 == 0:
+
+           print("Buzz")
+
+     else:
+   
+           print(num)
